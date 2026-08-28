54918988b970e86762a4398c3fdb8d55775e3504
thatsothoun <thatsothounctrl01@gmail.com>
2026-08-29 02:06:16 +0900

    add & fix lua config

--- plugin/autocmds.lua
+++ plugin/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    local hl_groups = vim.api.nvim_get_hl(0, {})
+    for key, hl in pairs(hl_groups) do
+      if hl.italic then
+        vim.api.nvim_set_hl(0, key, vim.tbl_extend("force", hl, { italic = false }))
+      end
+    end
+  end,
+})
